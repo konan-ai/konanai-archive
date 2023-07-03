6ef0a6616b53d94ced172af146f379f48b633821
KonanAI <110348966+konan-ai@users.noreply.github.com>
2023-07-03 13:03:27 -0400

    Update lumache.py

--- docs/source/lumache.py
+++ docs/source/lumache.py
@@ -1,7 +1,7 @@
 """
 Lumache - Python library for cooks and food lovers.
-    
-HELLO HELLO WORLD
+
+HELLO WORLD
 """
 
 __version__ = "0.1.0"
@@ -15,8 +15,8 @@ class InvalidKindError(Exception):
 def get_random_ingredients(kind=None):
     """
     Return a list of random ingredients as strings.
-    
-    HELLO WORLD
+
+    HELLO HELLO WORLD
 
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
@@ -25,36 +25,3 @@ def get_random_ingredients(kind=None):
     :rtype: list[str]
     """
     return ["shells", "gorgonzola", "parsley"]
-
-
-def not_random_ingredients(kind=None):
-    """
-    Return a list of random ingredients as strings.
-
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
-    """
-    return ["shells", "gorgonzola", "parsley"]
-
-
-def Conv1d(object):
-    """Applies a 1D convolution over an input signal composed of several input
-    planes.
-
-    In the simplest case, the output value of the layer with input size
-    :math:`(N, C_{\text{in}}, L)` and output :math:`(N, C_{\text{out}}, L_{\text{out}})` can be
-    precisely described as:
-
-    .. math::
-        \text{out}(N_i, C_{\text{out}_j}) = \text{bias}(C_{\text{out}_j}) +
-        \sum_{k = 0}^{C_{in} - 1} \text{weight}(C_{\text{out}_j}, k)
-        \star \text{input}(N_i, k)
-
-    where :math:`\star` is the valid `cross-correlation`_ operator,
-    :math:`N` is a batch size, :math:`C` denotes a number of channels,
-    :math:`L` is a length of signal sequence.
-    """
-    return
