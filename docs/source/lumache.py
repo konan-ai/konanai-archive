898b53ef448be88ce12db6c4f66e0f9c8a5350db
KonanAI <konan@konanai.com>
2023-07-03 00:38:38 -0400

    Update lumache troubleshooting

---
+++ docs/source/lumache.py
@@ -0,0 +1,43 @@
+"""
+Lumache - Python library for cooks and food lovers.
+"""
+
+__version__ = "0.1.0"
+
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
+
+
+def get_random_ingredients(kind=None):
+    """
+    Return a list of random ingredients as strings.
+
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
+    """
+    return ["shells", "gorgonzola", "parsley"]
+
+
+class Conv1d(object):
+    __doc__ = r"""Applies a 1D convolution over an input signal composed of several input
+    planes.
+
+    In the simplest case, the output value of the layer with input size
+    :math:`(N, C_{\text{in}}, L)` and output :math:`(N, C_{\text{out}}, L_{\text{out}})` can be
+    precisely described as:
+
+    .. math::
+        \text{out}(N_i, C_{\text{out}_j}) = \text{bias}(C_{\text{out}_j}) +
+        \sum_{k = 0}^{C_{in} - 1} \text{weight}(C_{\text{out}_j}, k)
+        \star \text{input}(N_i, k)
+
+    where :math:`\star` is the valid `cross-correlation`_ operator,
+    :math:`N` is a batch size, :math:`C` denotes a number of channels,
+    :math:`L` is a length of signal sequence.
+    """
+    pass
